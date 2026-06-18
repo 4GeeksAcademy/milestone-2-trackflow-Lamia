fc1b8fcd6ef38e9773f848bf61336043e5e0661c
Lamia-bouzari <lamia.bouzari29@gmail.com>
2026-06-18 14:59:39 +0000

    milestone-2-trackflow-m

---
+++ src/types/models.ts
@@ -0,0 +1,11 @@
+export type ProductCategory =
+|"Fashion"
+|"Electronics"
+|"Cosmetics"
+|"Home"
+|"Other";
+export type WarehouseLocation = "Los Angles" | "Zaragoza";
+export type ProductStatus =
+|"Active"
+|"Low stock"
+|"Out of stock"
